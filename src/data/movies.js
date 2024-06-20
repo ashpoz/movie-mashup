ca88caa30fc7ca7c82dd1356ec2a6ba7228a3fe3
Adrian Haynes <majorx3music@gmail.com>
2024-06-20 13:07:06 -0400

    Attempting to integrate netlify

--- src/data/movies.js
+++ src/data/movies.js
@@ -21,7 +21,7 @@ const movieData = [
         }
       }
     ],
-    "synopsis": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, veritatis similique. Corrupti ea voluptatem accusantium. Doloribus, nesciunt delectus similique eligendi dolores ducimus dignissimos necessitatibus ut nulla minus nobis quidem expedita?",
+    "synopsis": "Four women reminisce on one eventful summer in the 1970s when they were 12 years old. One of the girls fathers is a U.S. Army Officer serving in the Vietnam War who is tasked with killing a renegade Colonel who thinks he is a god.",
     "hints": []
   },
   {
@@ -46,7 +46,32 @@ const movieData = [
         }
       }
     ],
-    "synopsis": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, veritatis similique. Corrupti ea voluptatem accusantium. Doloribus, nesciunt delectus similique eligendi dolores ducimus dignissimos necessitatibus ut nulla minus nobis quidem expedita?",
+    "synopsis": "The aging patriarch of a mafia family is reluctant to the idea of his oldest daughter's upcoming wedding. Hilarity (and violence) insues.",
+    "hints": []
+  },
+  {
+    "id": 3,
+    "answer": "Airbud",
+    "sharedWord": "Air",
+    "movies": [
+      {
+        "title": "Con Air",
+        "year": "",
+        "image": {
+          "alt": "",
+          "url": ""
+        }
+      },
+      {
+        "title": "Air Bud",
+        "year": "",
+        "image": {
+          "alt": "",
+          "url": ""
+        }
+      }
+    ],
+    "synopsis": "Newly-paroled ex-con and former U.S. Ranger finds himself trapped in a prisoner-transport plane when the passengers seize control. The plane crashes in a small town, and he assumes a new identity, which is a coach of a school basketball team. An unexpected player joins the team - a circus dog who escaped from a cruel master.",
     "hints": []
   }
 ]
