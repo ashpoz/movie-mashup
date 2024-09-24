23d3dbbaa84b115a8bb446a28abcd84b2993e4ab
Adrian Haynes <majorx3music@gmail.com>
2024-09-23 22:14:29 -0400

    Update reveal button to a more subtle link

--- src/components/GameComponent.vue
+++ src/components/GameComponent.vue
@@ -128,6 +128,27 @@ button[type="submit"] {
   }
 }
 
+#revealButton {
+  display: block;
+  background: transparent;
+  border: none;
+  color: var(--color-text);
+  font-size: 1.25rem;
+  font-weight: bold;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 2rem;
+  padding: 0;
+  text-decoration: underline;
+  text-align: center;
+}
+
+@media (min-width: 768px) {
+  #gameWon {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
 .btn-group .btn {
   flex: 1;
 }
@@ -170,12 +191,6 @@ button[type="submit"] {
             <ErrorMessage v-show="formMessage" :message="formMessage" />
 
             <div class="btn-group">
-              <button @click="revealAnswer" ref="reveal" class="btn btn-secondary">
-                <span>Reveal Answer?</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
-                </svg>
-              </button>
               <!-- <button ref="hint" class="btn btn-secondary">
                 <span>Get a Hint</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -184,6 +199,7 @@ button[type="submit"] {
               </button> -->
             </div>
             <button ref="submit" type="submit" class="btn btn-primary">Enter</button>
+            <button id="revealButton" href="" @click="revealAnswer">Reveal Answer?</button>
           </fieldset>
         </form>
       </div>
